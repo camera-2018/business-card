89b558285d18981a30349dc1e5bf56d4128ae684
camera-2018 <2907618001@qq.com>
2023-01-24 23:53:31 +0800

    feat: a demo

--- src/popup/Popup.vue
+++ src/popup/Popup.vue
@@ -1,22 +1,32 @@
-<script setup lang="ts">
-import { storageDemo } from '~/logic/storage'
+<script setup>
+import { useImage } from '@vueuse/core'
+const avatarUrl = 'https://s1.ax1x.com/2023/01/23/pSYuIYD.png'
+const { isLoading } = useImage({ src: avatarUrl })
 
-function openOptionsPage() {
-  browser.runtime.openOptionsPage()
+const to_left = () => {
+  document.getElementsByClassName('img')[0].classList.remove('toright')
+}
+const to_right = () => {
+  document.getElementsByClassName('img')[0].classList.add('toright')
 }
 </script>
 
 <template>
-  <main class="w-[300px] px-4 py-5 text-center text-gray-700">
-    <Logo />
-    <div>Popup</div>
-    <SharedSubtitle />
-
-    <button class="btn mt-2" @click="openOptionsPage">
-      Open Options
-    </button>
-    <div class="mt-2">
-      <span class="opacity-50">Storage:</span> {{ storageDemo }}
+  <main class="w-[384px] h-[256px] text-center text-light-700 bg-[#138385] font-sans">
+    <div class="h-100% w-100% flex flex-row justify-between p-5 items-center">
+      <span v-if="isLoading" class="animate-pulse bg-[#0A4445] w-35 h-35 text-center font-black leading-34">Loading</span>
+      <img v-else :src="avatarUrl" class="w-35 h-35 img duration-700">
+      <div />
     </div>
+    <footer class="bg-[#138385] ">
+      <button class="i-carbon-caret-left hover:bg-[#1DCED1] bg-light-50" @click="to_left" />
+      <button class="i-carbon-caret-right hover:bg-[#1DCED1] bg-light-50" @click="to_right" />
+    </footer>
   </main>
 </template>
+
+<style>
+.toright{
+  @apply translate-x-142% transition
+}
+</style>
